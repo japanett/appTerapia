d8a98eabf955f758eee8c9986bb26ccf72927e85
japanett <gabrielkenzo@gmail.com>
2018-11-08 16:41:00 -0200

    dockerfile

--- src/pages/game-detail/game-detail.ts
+++ src/pages/game-detail/game-detail.ts
@@ -10,10 +10,27 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'game-detail.html',
 })
 export class GameDetailPage {
-  public game: any
+  public game: any;
+  public configuration: string;
+
 
   constructor(public modalCtrl: ModalController, public navCtrl: NavController, public viewCtrl: ViewController, public storage: Storage, public navParams: NavParams, private toast: ToastController, private userProvider: UsersProvider) {
-  this.game = navParams.get('game');
+    this.game = navParams.get('game');
+  }
+
+  configurationUsed(game) {
+    let _configuration;
+    let _gameConfig = game.config;
+    if (game.title == 'Jogo da Mercearia') {
+      let primeiraEtapa,
+        segundaEtapa,
+        terceiraEtapa;
+      
+      _configuration = 'Primeira Etapa: ' + primeiraEtapa + ', Segunda Etapa: ' + segundaEtapa + ', Terceira Etapa: ' + terceiraEtapa;
+    }
+    else if (game.title == 'Bola na Caixa'){
+
+    }
   }
 
   dismiss() {
@@ -22,7 +39,6 @@ export class GameDetailPage {
 
   ionViewDidLoad() {
     console.log(this.game);
-    console.log('ionViewDidLoad GameDetailPage');
   }
 
 }
