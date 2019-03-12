4bafd30ff26df50bdf0209dcca4c7698b52e4629
Gabriel Kenzo Hirata Camargo <am53_gcamargo@uolinc.com>
2019-03-12 16:46:34 -0300

    changes to pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -54,8 +52,6 @@ import { LoadingProvider } from '../providers/loading/loading';
     PacientEditPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     UsersProvider,
     LoadingProvider
